facaa95dfcaa82fa6a0a6fa16369067388da5451
Aitor Paredes <aitor.paredes@student.hogent.be>
2022-10-13 13:47:32 +0200

    Initial COmmit

---
+++ tests/DeckStubs/PlayerWinsDealerBurnsDeck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Tests.DeckStubs {
+    class PlayerWinsDealerBurnsDeck : Deck {
+
+        public PlayerWinsDealerBurnsDeck()
+        {
+            _cards = new List<BlackJackCard>
+            {
+                // Dealer
+                new BlackJackCard(Suit.Clubs, FaceValue.Seven),
+                new BlackJackCard(Suit.Clubs, FaceValue.Seven),
+            
+                // Player
+                new BlackJackCard(Suit.Clubs, FaceValue.Nine),
+                new BlackJackCard(Suit.Clubs, FaceValue.Nine),
+
+                // Dealer
+                new BlackJackCard(Suit.Clubs, FaceValue.Ten),
+            };
+        }
+    }
+}
