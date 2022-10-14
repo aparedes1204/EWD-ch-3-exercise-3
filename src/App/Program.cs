df3ad6ac50e1bba1f126c99c7ee7fcac7c6b907d
Aitor Paredes <aitor.paredes@student.hogent.be>
2022-10-14 17:19:07 +0200

    Program.cs added to app

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+do
+{
+    BlackJack bj = new BlackJack();
+    DisplayHandsAndTotals(bj.DealerHand, bj.PlayerHand);
+    while (bj.GameState != GameState.GameOver)
+    {
+        if (PlayerWantsCard())
+            bj.GivePlayerAnotherCard();
+        else
+            bj.PassToDealer();
+        DisplayHandsAndTotals(bj.DealerHand, bj.PlayerHand);
+    }
+    Console.WriteLine("Game ends: {0}", bj.GameSummary());
+} while (PlayerWantsAnotherGame());
+
+static bool PlayerWantsCard()
+{
+    int choice = 0;
+    while (choice < 1 || choice > 2)
+    {
+        Console.WriteLine("Enter your choice: ");
+        Console.WriteLine("1. Another card ");
+        Console.WriteLine("2. Pass ");
+        choice = int.Parse(Console.ReadLine());
+    }
+    return choice == 1;
+}
+
+static bool PlayerWantsAnotherGame()
+{
+    char c = '?';
+    while (c != 'y' && c != 'n')
+    {
+        Console.WriteLine("Do you want to play again? (y/n)");
+        c = char.Parse(Console.ReadLine());
+    }
+    return c == 'y';
+}
+
+static void DisplayHandsAndTotals(Hand dealerHand, Hand playerHand)
+{
+    Console.WriteLine();
+    Console.WriteLine("================");
+    Console.WriteLine(MapHand(dealerHand.Cards));
+    Console.WriteLine("Dealer total = {0}", dealerHand.Value);
+    Console.WriteLine();
+    Console.WriteLine(MapHand(playerHand.Cards));
+    Console.WriteLine("Player total = {0}", playerHand.Value);
+    Console.WriteLine("================");
+    Console.WriteLine();
+}
+
+static string MapHand(IEnumerable<BlackJackCard> hand)
+{
+    StringBuilder sb = new StringBuilder();
+    foreach (BlackJackCard card in hand)
+    {
+        if (card.FaceUp)
+            sb.Append(string.Format("-{0}/{1}-", card.Suit.ToString(), card.FaceValue.ToString()));
+        else
+            sb.Append("-?/?-");
+    }
+    return sb.ToString();
+}
